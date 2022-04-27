50849e8eebe6f0f51e2b2f23839b9f72b86b3522
Shreya <87652405+ShreyaSudheer@users.noreply.github.com>
2022-04-27 18:30:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-noseX = 0;
-noseY = 0;
-function preload(){
-}
-function setup(){
+leftWristX = "";
+rightWristX = "";
+
+function preload(){}
+
+function setup()
+{
     video = createCapture(VIDEO);
     video.size(500,550);
 
@@ -12,21 +14,26 @@ function setup(){
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
-function draw(){
-    background('#987ef7');
-    fill('#77e4f2');
-    stroke('#262aff');
-    square(noseX, noseY, 100);
+
+function draw()
+{   
+    var colory = "";
+    var wordy = "";
+    background("#896ded");
+    text(wordy, 50, 400);
+    fill(colory);
 }
-function modelLoaded(){
+function modelLoaded()
+{
     console.log("PoseNet is Initialized");
 }
-function gotPoses(results){
+function gotPoses(results)
+{
     if(results.length > 0)
     {
         console.log(results);
-        noseX = results[0].pose.nose.x;
-        noseY =  results[0].pose.nose.y;
-        console.log(" noseX = " +noseX+ " noseY = " +noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX =  results[0].pose.rightWrist.x;
+        console.log(" leftwristX = " +leftWristX+ " rightwristX = " +rightWristX);
     }
 }
